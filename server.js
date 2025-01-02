d00a525cac56e109b1cec679bff4a80fe7769b85
vivek <vivekck12343@gmail.com>
2025-01-02 16:36:31 +0530

    made cors to acces from any domian

--- server.js
+++ server.js
@@ -22,9 +22,23 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
+// app.use(
+//   cors({
+//     origin: process.env.CLIENT_BASE_URL,
+//     methods: ["GET", "POST", "DELETE", "PUT"],
+//     allowedHeaders: [
+//       "Content-Type",
+//       "Authorization",
+//       "Cache-control",
+//       "Expires",
+//       "Pragma",
+//     ],
+//     credentials: true,
+//   })
+// );
 app.use(
   cors({
-    origin: process.env.CLIENT_BASE_URL,
+    origin: "*", // Allow all domains and IP addresses
     methods: ["GET", "POST", "DELETE", "PUT"],
     allowedHeaders: [
       "Content-Type",
@@ -33,7 +47,7 @@ app.use(
       "Expires",
       "Pragma",
     ],
-    credentials: true,
+    credentials: true, // Optional, if you need to allow cookies to be sent with requests
   })
 );
 
